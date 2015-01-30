5e813c3182536e366ea12fb2b82f2a0881cb119e
HanxunHuangLemonBear <curtishuang@foxmail.com>
2015-01-29 20:04:46 -0500

    GetURLContent

---
+++ GetURLContent.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.io.*;
+
+public class GetURLContent {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        try{
+            open_url_file("https://boilerlink.purdue.edu/Organizations");
+        }catch(Exception e){
+            System.out.println("MalformedURLException");
+        }
+    }
+    public static void open_url_file(String target) throws Exception{
+        URL oracle = new URL(target);
+        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null){
+            System.out.println(inputLine);
+        }
+    }
+}
