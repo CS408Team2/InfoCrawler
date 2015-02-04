f18e60217b86183497a72aff1d72f5fccd159a36
Hanxun Huang <curtishuang@foxmail.com>
2015-02-04 18:11:43 -0500

    Add SearchSettingClass

--- InfoCrawler.java
+++ InfoCrawler.java
@@ -17,6 +17,16 @@ public class InfoCrawler {
         input_string = http_check(input_string);
         return input_string;
     }
+    
+    public static String input_keyword(){
+        Console console = System.console();
+        System.out.print("keyword:");
+        String input_string = console.readLine();
+        regular_command_check(input_string);
+        input_string = http_check(input_string);
+        return input_string;
+    }
+    
     public static void regular_command_check(String s){
         if(s.equals("-q")){
             System.exit(0);
@@ -45,21 +55,42 @@ public class InfoCrawler {
         // Repeat Mode 1
         // Periodic Mode 2
         String input_string;
+        String start_keyword;
+        String end_keyword;
+        System.out.println("\n======Please Select Mode======\n-r Repeat Mode\n-p Periodic Mode");
         input_string = input_command();
         if(input_string.equals("-r")){
             mode = 1;
         }else if(input_string.equals("-p")){
             mode = 2;
         }else{
-            System.out.println("\n=====Please enter -p or -r=====\n-r Repeat Mode\n-p Periodic Mode");
+            System.out.println("Error:Please select from available mode");
         }
         return mode;
     }
-    public static void setup(){
+    
+    public static int method_selection(){
+        Console console = System.console();
+        int method = 0;
+        String input_string;
+        System.out.println("\n======Please Select Available Method======\n-w Word by Word\n-r Regular Expression");
+        input_string = input_command();
+        if(input_string.equals("-w")){
+            method = 1;
+        }else if(input_string.equals("-r")){
+            method = 2;
+        }else{
+            System.out.println("Error:Please select from available method");
+        }
+        return method;
+    }
+    
+    public static SearchSetting setup(){
+        SearchSetting set = new SearchSetting();
         Console console = System.console();
         String URL_file;
         String input_string;
-        String BaseURL;//Base URL with increment variable in -r mode
+        String BaseURL = "";//Base URL with increment variable in -r mode
         //Repeat Mode Increment Range
         int increment_from = 0;
         int increment_to = 0;
@@ -68,6 +99,7 @@ public class InfoCrawler {
         int time_interval = 0;
         GetURLContent in = new GetURLContent();
         int mode = 0;
+        int method = 0;
         while (mode==0){
             mode = mode_selection();
         }
@@ -88,31 +120,43 @@ public class InfoCrawler {
                     range_set=1;
                 }catch(Exception e){
                     System.out.println("Please input a Integer");
-                
                 }
             }
-            for(i=increment_from;i<increment_to;i++){
-                
+            while(method==0){
+                method = method_selection();
             }
-        }else if(mode==2){
+        }
+        else if(mode==2){
             //Periodic Mode
             System.out.println("\n======Please enter the URL=====");
             BaseURL = input_URL();
             System.out.println("\n======Input Time Interval======");
-            System.out.print("Seconds:");
             int interval_time_set = 0;
-            while(interval_time_set==0){
+            while(interval_time_set == 0){
                 try{
+                    System.out.print("Seconds:");
                     input_string = console.readLine();
                     time_interval = Integer.parseInt(input_string);
+                    interval_time_set = 1;
                 }catch(Exception e){
                     System.out.println("Please input a Integer");
                 }
             }
+            while(method==0){
+                method = method_selection();
+            }
         }
+        set.mode = mode;
+        set.increment_to = increment_to;
+        set.increment_from = increment_from;
+        set.increment_times = increment_from - increment_to;
+        set.time_interval = time_interval;
+        set.BaseURL = BaseURL;
+        return set;
     }
     public static void main(String[] args) {
-        System.out.println("Welcome to InfoCrawler\n-h For help\n-q Exit\n======Please Select Mode======\n-r Repeat Mode\n-p Periodic Mode");
-        setup();
+        System.out.println("=====Welcome to InfoCrawler=====\n-h For help\n-q Exit");
+        SearchSetting set = setup();
+        System.out.println(set.BaseURL);
     }
 }
--- SearchSetting.java
+++ SearchSetting.java
@@ -1,31 +1,25 @@
 class SearchSetting {
-    class RepeatMode {
-        public String BaseURL;
-        public int increment_from = 0;
-        public int increment_to = 0;
-        public int increment_times = 0;
-        public int marks;
-        public String get_BaseURL() {
-            return BaseURL;
-        }
-        public int get_increment_from() {
-            return increment_from;
-        }
-        public int get_increment_to() {
-            return increment_to;
-        }
-        public int get_increment_times() {
-            return increment_times;
-        }
+    public String BaseURL;
+    public String start_keyword;
+    public String end_keyword;
+    public int mode = 0;
+    public int increment_from = 0;
+    public int increment_to = 0;
+    public int increment_times = 0;
+    public int time_interval = 0;
+    public String get_BaseURL() {
+        return BaseURL;
     }
-    class PeriodicMode {
-        public String BaseURL;
-        public int time_interval = 0;
-        public String get_BaseURL() {
-            return BaseURL;
-        }
-        public int get_time_interval() {
-            return time_interval;
-        }
+    public int get_increment_from() {
+        return increment_from;
+    }
+    public int get_increment_to() {
+        return increment_to;
+    }
+    public int get_increment_times() {
+        return increment_times;
+    }
+    public int get_time_interval() {
+        return time_interval;
     }
 }
