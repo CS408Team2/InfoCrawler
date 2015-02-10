8408723c55e7b340cc93ab776935d826b8ba511e
go307 <Kai@Kais-MacBook-Air.local>
2015-02-09 22:20:02 -0500

    regex search update

--- RegularExpressionSearch.java
+++ RegularExpressionSearch.java
@@ -5,7 +5,18 @@ import java.util.regex.Pattern;
 
 
 public class RegularExpressionSearch {
-    public static String RegularExpressionSearch(String input, String key1, String key2){
+    public static String[] RegularExpressionSearch(String input, String key1, String key2){
+        
+        String result = "";
+        
+        // If input only contain key1 + key 2, return
+        
+        String Key1AndKey2 = key1+key2;
+        if(input == Key1AndKey2){
+            return "";
+        }
+        
+        // Get how many times key1 and key2 does input string contain
         
         String regexKey1 = "\\b" + key1 + "\\b";
         String regexKey2 = "\\b" + key2 + "\\b";
@@ -28,8 +39,27 @@ public class RegularExpressionSearch {
         
         System.out.println(countKey1);
         System.out.println(countKey2);
+
+        // Get min number of countKey1 and countKey2
         
-        return "asd";
+        int min = getMin(countKey1, countKey2);
+        
+        //
+        
+        String result[] = new String[min];
+        
+        
+        
+        
+        return result;
+    }
+    
+    public static int getMin(int num1, int num2){
+        if(num1 <= num2){
+            return num1;
+        }else{
+            return num2;
+        }
     }
 
 }
