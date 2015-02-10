39f3cbba49f94f3f8443af5d5116d1ab26b980a8
go307 <Kai@Kais-MacBook-Air.local>
2015-02-09 22:01:55 -0500

    Start reject search

--- RegularExpressionSearch.java
+++ RegularExpressionSearch.java
@@ -1,7 +1,35 @@
+import java.net.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class RegularExpressionSearch {
-    public static String RegularExpressionSearch(String input_string, String keyword1, String keyword2){
-        String result_string = "";
-        return result_string;
+    public static String RegularExpressionSearch(String input, String key1, String key2){
+        
+        String regexKey1 = "\\b" + key1 + "\\b";
+        String regexKey2 = "\\b" + key2 + "\\b";
+        
+        Pattern pKey1 = Pattern.compile(regexKey1);
+        Matcher mKey1 = pKey1.matcher(input);
+        
+        Pattern pKey2 = Pattern.compile(regexKey2);
+        Matcher mKey2 = pKey2.matcher(input);
+        
+        int countKey1 = 0;
+        int countKey2 = 0;
+        
+        while(mKey1.find()) {
+            countKey1++;
+        }
+        while(mKey2.find()){
+            countKey2++;
+        }
+        
+        System.out.println(countKey1);
+        System.out.println(countKey2);
+        
+        return "asd";
     }
 
 }
