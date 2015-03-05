4e66fd10f48848d75ac574a1daf9cf5d86bcb3d2
YiJieWu <yijiewuboilerup@gmail.com>
2015-03-05 16:58:28 -0500

    Update SearchResult.java

--- SearchResult.java
+++ SearchResult.java
@@ -1,6 +1,50 @@
 class SearchResult {
-    public String result_string;
-    public String[] result_array;
-    public long time_used;
-    public int count;
+    private String result_string;
+    private String[] result_array;
+    private long time_used;
+    private int count;
+    
+    public SearchResult(){
+        result_string="";
+        result_array={};
+        time_used=0;
+        count=0;
+    }
+    
+    public SearchResult(String a,String[]b,int c,int d){
+        result_string=a;
+        result_array=b;
+        time_used=c;
+        count=d;
+        
+    }
+    public String getrs(){
+        return result_string;
+    }
+    public String [] getra(){
+        return result_array;
+    }
+    
+    public int gettu(){
+        return time_used;
+    }
+    
+    public int getcount(){
+        return count;
+    }
+    
+    public setrs(String a){
+        result_string=a;
+        
+    }
+    public setra(String[] a){
+        
+        result_array=a;
+    }
+    public settime(int a){
+        time_used=a;
+    }
+    public setcount(int a){
+        count=a;
+    }
 }
