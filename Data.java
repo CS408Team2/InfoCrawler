6b0c150a809d1e63f5dac1cb4da45f9adc689b95
YiJieWu <yijiewuboilerup@gmail.com>
2015-03-09 12:40:58 -0400

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+public class Data{
+
+  // JDBC driver name and database URL
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost/EMP";
+
+   //  Database credentials
+   static final String USER = "username";
+   static final String PASS = "password";
+   
+   public static void main(String[] args) {
+   Connection conn = null;
+   Statement stmt = null;
+   try{
+      //STEP 2: Register JDBC driver
+      Class.forName("com.mysql.jdbc.Driver");
+
+      //STEP 3: Open a connection
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+  
+
+
+
+
+
+
+}
