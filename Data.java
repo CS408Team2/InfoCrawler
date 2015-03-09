5c2c8f2e473473d3e00f51190cec40ef1174eaaa
YiJieWu <yijiewuboilerup@gmail.com>
2015-03-09 12:56:27 -0400

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,30 +1,45 @@
 import java.sql.*;
 public class Data{
-
-  // JDBC driver name and database URL
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost/EMP";
-
-   //  Database credentials
-   static final String USER = "username";
-   static final String PASS = "password";
-   
-   public static void main(String[] args) {
-   Connection conn = null;
-   Statement stmt = null;
-   try{
-      //STEP 2: Register JDBC driver
-      Class.forName("com.mysql.jdbc.Driver");
-
-      //STEP 3: Open a connection
-      System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
+    
+    
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    private String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+    private String USER = "username";
+    private String PASS = "password";
   
+  
+    public void set(String a,String b,String c){
+        DB_URL=a;
+        USER=b;
+        Pass=c;
+    }
+  
+  
+  public void start(){
 
-
-
-
-
+   
+         Connection conn = null;
+         try{
+         //STEP 2: Register JDBC driver
+              Class.forName("com.mysql.jdbc.Driver");
+      
+         //STEP 3: Open a connection
+              conn = DriverManager.getConnection(DB_URL, USER, PASS);
+          }catch(SQLException se){
+                 se.printStackTrace();
+          }catch(Exception e){
+                e.printStackTrace();
+          }finally{
+          try{
+            if(conn!=null)
+              conn.close();
+             }catch(SQLException se){
+                  se.printStackTrace();
+             }//end finally try
+          }//end try
+    }//end of Start method
+   
+   
 
 }
